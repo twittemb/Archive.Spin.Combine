0f4271d644159808202c99f197349bb570ecfea6
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2019-08-17 22:23:31 -0400

    Initial Commit

---
+++ Spin.Combine/Spin_Combine.h
@@ -0,0 +1,19 @@
+//
+//  Spin_Combine.h
+//  Spin.Combine
+//
+//  Created by Thibault Wittemberg on 2019-08-17.
+//  Copyright © 2019 Thibault Wittemberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Spin_Combine.
+FOUNDATION_EXPORT double Spin_CombineVersionNumber;
+
+//! Project version string for Spin_Combine.
+FOUNDATION_EXPORT const unsigned char Spin_CombineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spin_Combine/PublicHeader.h>
+
+
